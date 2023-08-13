647e9aa3df38a8f191d25fd2a01901c27e633e6d
liccch <shoutou@189.cn>
2023-08-13 18:50:13 +0800

    提交消息

---
+++ create_wg_service.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SERVICE_NAME="wg-quick-service"
+DESCRIPTION="启动 WireGuard 配置"
+COMMAND="wg-quick up ./wghub.conf"
+USER="root"
+
+echo "[Unit]
+Description=$DESCRIPTION
+After=network.target
+
+[Service]
+User=$USER
+ExecStart=$COMMAND
+
+[Install]
+WantedBy=multi-user.target" | sudo tee /etc/systemd/system/$SERVICE_NAME.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable $SERVICE_NAME.service
+sudo systemctl start $SERVICE_NAME.service
+
+echo "服务 $SERVICE_NAME 已创建并启动！"
